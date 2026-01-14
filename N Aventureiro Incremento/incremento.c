5d1bd7def60213c7d6790e48c832d44a5ece066b
Francisco Junior <202511006313@alunos.estacio.br>
2026-01-14 00:56:06 +0000

    incremento

---
+++ N Aventureiro Incremento/incremento.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+/*
+Incremento (++)
+Pré-Incremento ++a
+Pós-Incremento a++
+Decremento (--)
+Pre-Decremento --a
+Pós-Decremento a--
+*/
+
+int numero1 = 1;
+
+prinf ("Antes incremento: %d\n", numero1);
+numero1++;
+prinf ("Apos incremento: %d\n", numero1);
+
+
+
+}
